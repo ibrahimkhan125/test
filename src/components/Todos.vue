86b83f04468d2a05076448960808795791f79b81
ibrahimkhan125 <m.ibrahim8882@gmail.com>
2022-11-13 00:05:27 +0500

    working on todos app

---
+++ src/components/Todos.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Todos</h1>
+        <div v-for='todo in todos' v-bind:key="todo.id">{{todo.title}}</div> 
+    </div>
+</template>
+<script>
+export default {
+    name: "TodoS",
+    props: ['todos']
+}
+</script>
+<style scoped>
+</style>
