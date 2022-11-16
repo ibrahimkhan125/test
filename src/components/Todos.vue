f4acbaa629e30f45b2469d585dfeffc95540a63a
ibrahimkhan125 <m.ibrahim8882@gmail.com>
2022-11-16 23:57:09 +0500

    Update Todos.vue

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,13 +1,21 @@
 <template>
     <div>
         <h1>Todos</h1>
-        <div v-for='todo in todos' v-bind:key="todo.id">{{todo.title}}</div> 
+        <div v-for='todo in todos' v-bind:key="todo.id">
+            <!-- {{todo.title}} -->
+            <TodoItem v-bind:todoP="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
+        </div>
+         
     </div>
 </template>
 <script>
+import TodoItem from './TodoItem.vue';
 export default {
     name: "TodoS",
-    props: ['todos']
+    props: ['todos'],
+    components: {
+        TodoItem
+    }
 }
 </script>
 <style scoped>
